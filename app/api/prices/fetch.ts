b79e955e77b3f72bc404313082d7c3785e1d86cd
Colin Franceschini <hello@colinfran.com>
2025-11-27 12:48:57 -0800

    Update fetch.ts

--- app/api/prices/fetch.ts
+++ app/api/prices/fetch.ts
@@ -46,10 +46,10 @@ function buildPriceMap(rows: any[]): Prices {
 export const fetchPrices = async (): Promise<Response<Prices>> => {
   try {
     console.log("Fetching prices from NASDAQ and NYSE")
-    const nasdaq = await fetch("https://api.nasdaq.com/api/screener/stocks?exchange=NASDAQ").then(
+    const nasdaq = await fetch("https://api.nasdaq.com/api/screener/stocks?exchange=NASDAQ&limit=10000").then(
       (r) => r.json(),
     )
-    const nyse = await fetch("https://api.nasdaq.com/api/screener/stocks?exchange=NYSE").then((r) =>
+    const nyse = await fetch("https://api.nasdaq.com/api/screener/stocks?exchange=NYSE&limit=10000").then((r) =>
       r.json(),
     )
     const nasdaqRows = nasdaq.data.table.rows
